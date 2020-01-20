e5805587c80bcf4872fc50dc8dc23302e5549e3c
farguedas <francisco.arguedas@firstfactory.com>
2020-01-20 16:45:01 -0600

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jenkinsApp
+{
+    [TestFixture]
+    class UnitTest1
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void initDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.com/";
+        }
+
+        [TestCase("zapato")]
+        [TestCase("junit")]
+        public void search(String text)
+        {
+            driver.FindElement(By.Name("q")).SendKeys(text);
+        }
+
+        [TestCase("zapato")]
+        [TestCase("junit")]
+        public void searchWithClick(String text)
+        {
+            driver.FindElement(By.Name("q")).SendKeys(text);
+            //driver.FindElement(By.Name("btnK")).Click();
+        }
+
+        [TearDown]
+        public void quit()
+        {
+            driver.Quit();
+        }
+    }
+}
