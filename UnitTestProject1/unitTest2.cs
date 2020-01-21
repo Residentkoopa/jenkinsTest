d14c506aafa03b0dc451cb71814ef24b5c1f04c4
farguedas <francisco.arguedas@firstfactory.com>
2020-01-21 13:35:13 -0600

    adding firefox tests

---
+++ UnitTestProject1/unitTest2.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jenkinsApp
+{
+    [TestFixture]
+    class UnitTest2
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void initDriver()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.com/";
+        }
+
+        [TestCase("zapato")]
+        [TestCase("junit")]
+        public void search(String text)
+        {
+            driver.FindElement(By.Name("q")).SendKeys(text);
+        }
+
+        [TestCase("zapato")]
+        [TestCase("junit")]
+        public void searchWithClick(String text)
+        {
+            driver.FindElement(By.Name("q")).SendKeys(text);
+            //driver.FindElement(By.Name("btnK")).Click();
+        }
+
+        [TearDown]
+        public void quit()
+        {
+            driver.Quit();
+        }
+    }
+}
